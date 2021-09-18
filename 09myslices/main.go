677354edae61bee8ef677a20234b8d763f3d712b
jay75chauhan <chauhan75jayk@gmail.com>
2021-09-18 21:58:14 +0530

    myslice

---
+++ 09myslices/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+
+	
+	fmt.Println("hii into slice")
+    
+	var frut = []string{"a", "b", "c", "d", "e"}
+
+    fmt.Printf("hii into slice is %T \n ",frut)
+
+       frut = append( frut,"rr")
+       fmt.Println(frut)
+
+	   frut = append(frut[1:2])
+       fmt.Println(frut)
+
+
+
+	   score := make([]int,4)
+
+	   score[0] = 1
+	   score[1] = 3
+	   score[2] = 5
+	   score[3] = 2
+	//    score[4] = 255
+	   
+	score = append(score,44,66)
+
+     fmt.Println(score)
+
+    sort.Ints(score)
+    fmt.Println(score)
+    fmt.Println(sort.IntsAreSorted(score))
+
+
+}

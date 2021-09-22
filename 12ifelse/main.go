bdbd2eced120f12c20d27d671cf89997d92eaaf2
jay75chauhan <chauhan75jayk@gmail.com>
2021-09-22 22:09:01 +0530

    my if else

---
+++ 12ifelse/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+fmt.Println("hii ef Else")
+
+
+login :=255
+
+var result string
+
+if login < 10 {
+	result = "r"
+} else if login > 10 {
+	result = "no"
+}else {
+	result = "login"
+}
+
+
+fmt.Println(result)
+
+    if num := 3 ; num < 10 {
+		fmt.Println("hii ef ")
+	}else {
+		fmt.Println("hii ef ")
+	}
+
+	//  if num3 != nil {
+	// 	 fmt.Println("hii ef ")
+	//  }
+
+}

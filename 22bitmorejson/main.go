7d2bf08e357740b858006818f786e961e827c6ae
jay75chauhan <chauhan75jayk@gmail.com>
2021-11-02 21:51:37 +0530

    addon json decoding

--- 22bitmorejson/main.go
+++ 22bitmorejson/main.go
@@ -17,7 +17,8 @@ func main() {
 	fmt.Println("welcome to JSON ")
 
 
-	EncodeJson()
+	//EncodeJson()
+	DecodeJson()
 }
 
 
@@ -40,7 +41,48 @@ func EncodeJson() {
  if err != nil {
 	 panic(err)
  }
+ fmt.Println(finalJson)
  
  fmt.Printf("%s\n",finalJson)
 
 }
+
+
+func DecodeJson() {
+
+	jsonDatafrom := []byte(`
+	 {
+	 	 "name": "jay",
+	 	 "Price": 66,
+	 	 "tags": [ "wev-dev","33" ]
+	 }	
+	`)
+
+	var coursejay course 
+	
+	 checkValid :=  json.Valid(jsonDatafrom)
+
+
+
+	 if checkValid {
+             fmt.Println("JSON is walid")
+			 json.Unmarshal(jsonDatafrom, &coursejay)
+			 fmt.Printf("%#v\n",coursejay)
+	 } else {
+		  fmt.Println("JSON is NOT walid")
+	 }
+
+
+
+	 // some case where you just to add data to key values
+
+
+	 var mydata map[string]interface{}
+	 json.Unmarshal(jsonDatafrom, &mydata)
+	 fmt.Printf("%#v\n",mydata)
+
+	 for k, v := range mydata {
+		 fmt.Printf("key : %v, value : %v and type : %T\n", k, v,v)
+	 }
+
+} 
